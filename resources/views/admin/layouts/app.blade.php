a28263e0de346558b21d241f0f98b74bf3f947a8
dheandocx <doctodhean@gmail.com>
2024-03-12 14:43:17 +0800

    second commit

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,21 +1,16 @@
-
 <!DOCTYPE html>
 
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>AdminLTE 3 | Starter</title>
+<title> Prototype </title>
 
-<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
-
-<!--<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">-->
-
-<!--<link rel="stylesheet" href="dist/css/adminlte.min.css?v=3.2.0">-->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body class="hold-transition sidebar-mini dark-mode">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
 
@@ -156,7 +151,7 @@ Nora Silvester
 
 <a href="index3.html" class="brand-link">
 <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-<span class="brand-text font-weight-light">AdminLTE 3</span>
+<span class="brand-text font-weight-light">Biologic Systems <br> Computer Center</span>
 </a>
 
 <div class="sidebar accent">
@@ -170,7 +165,8 @@ Nora Silvester
 </div>
 </div>
 
-<div class="form-inline">
+<!-- SEARCH SECTION
+    <div class="form-inline">
 <div class="input-group" data-widget="sidebar-search">
 <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
 <div class="input-group-append">
@@ -179,43 +175,67 @@ Nora Silvester
 </button>
 </div>
 </div>
-</div>
+</div> -->
 
 <nav class="mt-2">
 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-<li class="nav-item menu-open">
-<a href="#" class="nav-link active">
+
+
+<li class="nav-item">
+<router-link to="/admin/dashboard" active-class="active" class="nav-link">
 <i class="nav-icon fas fa-tachometer-alt"></i>
 <p>
-Starter Pages
-<i class="right fas fa-angle-left"></i>
+Dashboard
 </p>
-</a>
-<ul class="nav nav-treeview">
-<li class="nav-item">
-<a href="#" class="nav-link active">
-<i class="far fa-circle nav-icon"></i>
-<p>Active Page</p>
-</a>
+</router-link>
 </li>
+
 <li class="nav-item">
-<a href="#" class="nav-link">
-<i class="far fa-circle nav-icon"></i>
-<p>Inactive Page</p>
-</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-<i class="nav-icon fas fa-th"></i>
+<router-link to="/admin/appointments" active-class="active" class="nav-link">
+<i class="nav-icon fas fa-calendar-alt"></i>
 <p>
-Simple Link
-<span class="right badge badge-danger">New</span>
+Appointments
+</p>
+</router-link>
+</li>
+
+<li class="nav-item">
+<router-link to="/admin/users" active-class="active" class="nav-link">
+<i class="nav-icon fas fa-users"></i>
+<p>
+Users
+</p>
+</router-link>
+</li>
+
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-cog"></i>
+<p>
+Settings
 </p>
 </a>
 </li>
+
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-user"></i>
+<p>
+Profile
+</p>
+</a>
+</li>
+
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-sign-out-alt"></i>
+<p>
+Logout
+</p>
+</a>
+</li>
+
 </ul>
 </nav>
 
@@ -224,8 +244,8 @@ Simple Link
 </aside>
 
 <div class="content-wrapper">
-
-<div class="content-header">
+    <router-view></router-view>
+<!-- <div class="content-header">
 <div class="container-fluid">
 <div class="row mb-2">
 <div class="col-sm-6">
@@ -296,7 +316,7 @@ content.
 </div>
 
 </div>
-</div>
+</div> -->
 
 </div>
 
